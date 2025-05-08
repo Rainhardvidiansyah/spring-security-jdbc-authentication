fcf13551111f0cbcd762f800d166aeacff8a5d45
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-05-08 21:07:58 +0700

    create two test file jwt auth filter test and jwt auth entry test

---
+++ src/test/java/com/security/jdbc/security/jwt/JwtAuthEntryTest.java
@@ -0,0 +1,42 @@
+package com.security.jdbc.security.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthEntryTest {
+
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private AuthenticationException authException;
+
+    @InjectMocks
+    private JwtAuthEntry jwtAuthEntry;
+
+    @Test
+    void commence() throws ServletException, IOException {
+        jwtAuthEntry.commence(request, response, authException);
+
+        Mockito.verify(response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized user cannot access the protected resource");
+    }
+
+
+
+}
