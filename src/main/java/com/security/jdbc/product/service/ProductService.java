362373269b64b0194682e97b8c275c1f661b94e7
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-05-20 16:07:52 +0700

    create product service class

---
+++ src/main/java/com/security/jdbc/product/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.security.jdbc.product.service;
+
+
+import com.security.jdbc.product.dto.request.CreateProductDtoRequest;
+import com.security.jdbc.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public CreateProductDtoRequest saveProduct(CreateProductDtoRequest productDtoRequest){
+        CreateProductDtoRequest productRequest = productRepository.insertProduct(productDtoRequest);
+        return productRequest;
+    }
+}
