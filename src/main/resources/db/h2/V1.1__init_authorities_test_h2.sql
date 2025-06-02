9fad03cc150abe660a89184fc9ffa9af66580a22
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-06-03 00:55:50 +0700

    change mysql driver to postgresql

--- src/main/resources/db/h2/V1.1__init_authorities_test_h2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE authorities (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    authorities_name VARCHAR(255) NOT NULL,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_date TIMESTAMP,
-    updated_by VARCHAR(255),
-    deleted_at TIMESTAMP
-);
--- src/main/resources/db/migration/prime/V1.4__Insert_data_into_table_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (email, password, username, enabled) VALUES
-('rainhard@email.com', '$2a$12$6u8Mp34rZLl7oONcEgeAHOBX1kdU7dCyXb7vPgR.6IvuWYV1u0iu.', 'alice', 1),
-('maulida@email.com', '$2a$12$6u8Mp34rZLl7oONcEgeAHOBX1kdU7dCyXb7vPgR.6IvuWYV1u0iu.', 'bobby', 1),
-('bintang@example.com', '$2a$12$6u8Mp34rZLl7oONcEgeAHOBX1kdU7dCyXb7vPgR.6IvuWYV1u0iu.', 'charlie_c', 0),
-('ratna@example.com', '$2a$12$6u8Mp34rZLl7oONcEgeAHOBX1kdU7dCyXb7vPgR.6IvuWYV1u0iu.', 'diana', 0);
