88ddfd579451505a296f870c589574649ad13927
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-06-04 16:41:41 +0700

    Added logger and changed inejection in UserServiceImpl 😎😎

--- src/main/java/com/security/jdbc/authentication/service/UserServiceImpl.java
+++ src/main/java/com/security/jdbc/authentication/service/UserServiceImpl.java
@@ -1,25 +1,31 @@
 package com.security.jdbc.authentication.service;
 
 
-import com.security.jdbc.authentication.repository.UserRepositoryImpl;
+import com.security.jdbc.authentication.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserServiceImpl implements UserService {
 
-    private final UserRepositoryImpl userRepositoryImpl;
+    private static Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepositoryImpl userRepositoryImpl, PasswordEncoder passwordEncoder) {
-        this.userRepositoryImpl = userRepositoryImpl;
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Override
     public String addUser(String email, String password) {
+        LOGGER.info("Add User method is hit...");
         String encodedPassword = passwordEncoder.encode(password);
-        this.userRepositoryImpl.addUser(email, encodedPassword);
+        this.userRepository.addUser(email, encodedPassword);
         return "User registered successfully!";
     }
 }
