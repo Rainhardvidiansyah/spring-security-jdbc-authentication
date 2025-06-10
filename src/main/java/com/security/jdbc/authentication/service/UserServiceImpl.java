850ef892850aa69896aa7bd395b9770f56c45cc0
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-06-10 10:46:14 +0700

    implements two method from UserService in UserServiceImpl ✅✅✅

--- src/main/java/com/security/jdbc/authentication/service/UserServiceImpl.java
+++ src/main/java/com/security/jdbc/authentication/service/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.security.jdbc.authentication.service;
 
 
+import com.security.jdbc.authentication.dto.UserInfo;
 import com.security.jdbc.authentication.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -27,5 +31,15 @@ public class UserServiceImpl implements UserService {
         String encodedPassword = passwordEncoder.encode(password);
         this.userRepository.addUser(email, encodedPassword);
         return "User registered successfully!";
+    }
+
+    @Override
+    public UserInfo findUserById(Long id) {
+        return userRepository.getOneUserById(id);
+    }
+
+    @Override
+    public List<GrantedAuthority> getAuthoritiesByEmail(String email) {
+        return userRepository.getUserAuthoritiesByUserEmail(email);
     }
 }
